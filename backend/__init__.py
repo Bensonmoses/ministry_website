b5d038f7021b25e535992e5bc17d9a2e34f34853
bensonn-moses-palparthi <bensonmoses123@outlook.com>
2025-02-01 16:49:45 -0500

    Redeploying Flask API

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,13 +3,12 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for frontend communication
+CORS(app)
 
-@app.route("/")
+@app.route("/api/message")
 def home():
     return jsonify({"message": "Ministry Backend is Running!"})
 
-# Azure Function entry point
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    """Azure Function entry point to handle HTTP requests"""
+    """Azure Function entry point"""
     return func.WsgiMiddleware(app.wsgi_app).handle(req)
