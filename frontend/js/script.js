84a0be31d9a5979e29ea8bb99185e39c47dffbf5
bensonn-moses-palparthi <bensonmoses123@outlook.com>
2025-02-02 13:21:12 -0500

    changes in code

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Ministry Website Loaded!");
-});
+function toggleMenu() {
+    var mobileNav = document.getElementById("mobileNav");
+    if (mobileNav.style.display === "flex") {
+        mobileNav.style.display = "none";
+    } else {
+        mobileNav.style.display = "flex";
+    }
+}
